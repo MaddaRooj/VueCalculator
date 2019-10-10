2b277bd0fb70ba66612256cc3a1f30e293efd2dc
Addam Joor <38664958+MaddaRooj@users.noreply.github.com>
2019-10-09 21:06:03 -0500

    added grid

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,12 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calculator from './components/Calculator.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Calculator
   }
 }
 </script>
